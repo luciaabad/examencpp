b3f2d37eeaba8165d4e93391610618b2fb1cb420
luciaabad <labadgut@myuax.com>
2024-11-05 11:05:51 +0100

    ejercicio 3

---
+++ Ejercicio3/calificaciones.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Lucia Abad on 5/11/24.
+//
+
+#include "calificaciones.h"
+#include <iostream>
+using namespace std;
+
+void calificaciones() {
+    cout << "Ingrese el numero la calificacion" << endl;
+
+
+
+
+}
---
+++ Ejercicio3/calificaciones.h
@@ -0,0 +1,16 @@
+//
+// Created by Lucia Abad on 5/11/24.
+//
+
+#ifndef CALIFICACIONES_H
+#define CALIFICACIONES_H
+
+
+
+class calificaciones {
+
+};
+
+
+
+#endif //CALIFICACIONES_H
